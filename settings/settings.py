9ea26da9591bd2be76ddc4e8092827c204b5421a
iBernin <berninivan@gmail.com>
2024-02-05 13:39:28 +0300

    black

--- settings/settings.py
+++ settings/settings.py
@@ -17,23 +17,19 @@ from yaml import safe_load
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-f = open(BASE_DIR / 'config.yaml', 'r')
+f = open(BASE_DIR / "config.yaml", "r")
 config = safe_load(f)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i=w!tyt-_zv1(0aj3%n_1@qwri1jhaw5c%4tjqs=jcy%0tx0g#'
+SECRET_KEY = "django-insecure-i=w!tyt-_zv1(0aj3%n_1@qwri1jhaw5c%4tjqs=jcy%0tx0g#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "0.0.0.0"
-]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
 INTERNAL_IPS = [
     # ...
@@ -44,51 +40,49 @@ INTERNAL_IPS = [
 
 INSTALLED_APPS = [
     # builtins
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # site-packages
     "debug_toolbar",
-
     # My packages
-    'repricer',
-    'users',
+    "repricer",
+    "users",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware"
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
-ROOT_URLCONF = 'settings.urls'
+ROOT_URLCONF = "settings.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'settings.wsgi.application'
+WSGI_APPLICATION = "settings.wsgi.application"
 
 
 # Database
@@ -112,33 +106,26 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': {
+    "default": {
         # String. It must be "mssql".
-        'ENGINE': 'mssql',
-
+        "ENGINE": "mssql",
         # String. Database name. Required.
-        'NAME': 'marketprod',
-
+        "NAME": "marketprod",
         # String. Database user name in "user" format. If not given then MS Integrated Security will be used.
-        'USER': config['marketserver_sql']['login']
-,
-
+        "USER": config["marketserver_sql"]["login"],
         # String. Database user password.
-        'PASSWORD': config['marketserver_sql']['pass'],
-
-         # String. SQL Server instance in "server\instance" format.
-        'HOST': '192.168.22.224',
-
+        "PASSWORD": config["marketserver_sql"]["pass"],
+        # String. SQL Server instance in "server\instance" format.
+        "HOST": "192.168.22.224",
         # String. Server instance port. An empty string means the default port.
-        'PORT': '',
-
+        "PORT": "",
         # Dictionary. Additional database settings.
-        'OPTIONS': {
+        "OPTIONS": {
             # String. ODBC Driver to use ("ODBC Driver 17 for SQL Server",
             # "SQL Server Native Client 11.0", "FreeTDS" etc).
             # Default is "ODBC Driver 17 for SQL Server".
             # 'Trusted_Connection': 'no',
-            'driver': 'ODBC Driver 17 for SQL Server',
+            "driver": "ODBC Driver 17 for SQL Server",
         },
     },
 }
@@ -148,16 +135,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -165,9 +152,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -177,19 +164,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CELERY_RESULT_BACKEND = "rpc://"
 CELERY_RESULT_PERSISTENT = True
 
 # login
-AUTH_USER_MODEL = 'users.MpUser'
+AUTH_USER_MODEL = "users.MpUser"
 
-LOGIN_URL = '/users/login/'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = "/users/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
